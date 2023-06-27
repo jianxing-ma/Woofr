0ba986bafcff6ac5d2a46e705dcd3e70ac4eb5e2
Jason Pikturna <Jason.Pikturna@tlgcohort.com>
2023-06-27 12:14:25 -0700

    Completed a working shell of the servicesDescription page

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Tooltips for hover over information on the services-description.html page
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
