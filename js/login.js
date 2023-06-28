9c0a16d3a5981e07b2d985d2a64e1528ba5b49f8
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-06-28 16:47:42 -0600

    pet names and user name populated by the new function populateUserAndPetData()

--- js/login.js
+++ js/login.js
@@ -1,19 +1,22 @@
-const pantryAPIBasketUserUrl = "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/USER_ACCOUNT_DATABASE";
+// import { loadUserPetsData } from "./my-account.js";
+const pantryAPIBasketUserUrl =
+  "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/USER_ACCOUNT_DATABASE";
 
 // check user login status, and update login related elements accordingly
-export function validateLogin(){
+export function validateLogin() {
+  if (typeof pills_register_form != "undefined") {
+    pills_register_form.addEventListener("submit", (e) =>
+      handleAccountRegister(e)
+    );
+  }
 
-    if (typeof pills_register_form != "undefined") {
-        pills_register_form.addEventListener("submit", (e) => handleAccountRegister(e));
-    }
+  if (typeof pills_login_form != "undefined") {
+    pills_login_form.addEventListener("submit", (e) => handleAccountLogin(e));
+  }
 
-    if (typeof pills_login_form != "undefined") {
-        pills_login_form.addEventListener("submit", (e) => handleAccountLogin(e));
-    }
-
-    if(JSON.parse(localStorage.getItem("userInfo"))) {
-        const profileButton = document.createElement("div");
-        profileButton.innerHTML = `
+  if (JSON.parse(localStorage.getItem("userInfo"))) {
+    const profileButton = document.createElement("div");
+    profileButton.innerHTML = `
         <div id="profile_btn" class="dropstart">
             <button
             class="btn btn-secondary dropdown-toggle"
@@ -30,76 +33,82 @@ export function validateLogin(){
                 <li><a id="btn_sign_out" class="dropdown-item">Sign Out</a></li>
             </ul>
         </div>
-        `
+        `;
 
-        login_btn_container.innerHTML = "";
-        login_btn_container.append(profileButton);
+    login_btn_container.innerHTML = "";
+    login_btn_container.append(profileButton);
 
+    // loadUserPetsData();
 
-        document.getElementById("btn_sign_out").addEventListener("click", () => {
-            localStorage.clear();
-        document.location.reload();
-        });
-    }else {
-        const loginButton = document.createElement("div");
-        loginButton.innerHTML = `
+    document.getElementById("btn_sign_out").addEventListener("click", () => {
+      localStorage.clear();
+      document.location.reload();
+    });
+  } else {
+    const loginButton = document.createElement("div");
+    loginButton.innerHTML = `
         <a id="login_btn" class="btn btn-primary" href="./login.html">Login</a>
-        `
-        login_btn_container.innerHTML = "";
-        login_btn_container.append(loginButton);
-    }
+        `;
+    login_btn_container.innerHTML = "";
+    login_btn_container.append(loginButton);
+  }
 }
 
 //___________________________________________________________________________
 //____________________________ Registration__________________________________
 //___________________________________________________________________________
 export function handleAccountRegister(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // get data from user
-    const registerName = getInputValue("register_mame");
-    const registerUsername = getInputValue("register_username");
-    const registerEmail = getInputValue("register_email");
-    const registerPassword = getInputValue("register_password");
-    const registerRepeatPassword = getInputValue("register_repeat_password");
+  // get data from user
+  const registerName = getInputValue("register_mame");
+  const registerUsername = getInputValue("register_username");
+  const registerEmail = getInputValue("register_email");
+  const registerPassword = getInputValue("register_password");
+  const registerRepeatPassword = getInputValue("register_repeat_password");
 
-    if (registerPassword === registerRepeatPassword) {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-    
-        let requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
+  if (registerPassword === registerRepeatPassword) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        fetch(pantryAPIBasketUserUrl, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            if (registerUsername in data) {
-                alert("Username is already taken!");
-            }else {
-                const userInfo = {"name": registerName, "username": registerUsername, "email": registerEmail, "password": registerPassword};
-        
-                localStorage.setItem("userInfo", JSON.stringify(userInfo));
-                updateUserInfoToDatabase(userInfo);
-        
-                alertRegistrationSuccess();
-            }
-        })
-        .catch(error => console.log('error', error));
-    }else {
-        alert("Passwords do not match!");
-    }
+    let requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(pantryAPIBasketUserUrl, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (registerUsername in data) {
+          alert("Username is already taken!");
+        } else {
+          const userInfo = {
+            name: registerName,
+            username: registerUsername,
+            email: registerEmail,
+            password: registerPassword,
+          };
+
+          localStorage.setItem("userInfo", JSON.stringify(userInfo));
+          updateUserInfoToDatabase(userInfo);
+
+          alertRegistrationSuccess();
+        }
+      })
+      .catch((error) => console.log("error", error));
+  } else {
+    alert("Passwords do not match!");
+  }
 }
 
 // Toast message to redirect user to account page following login
 function alertRegistrationSuccess() {
-    const alertMessage = document.createElement("div");
-    alertMessage.classList.add("d-flex");
-    alertMessage.classList.add("justify-content-center");
+  const alertMessage = document.createElement("div");
+  alertMessage.classList.add("d-flex");
+  alertMessage.classList.add("justify-content-center");
 
-    alertMessage.innerHTML = `
+  alertMessage.innerHTML = `
     <div class="card" style="width: 18rem;">
         <img src="https://tlg-23-04-sde-2.github.io/Team-Woofr/images/dog_welcome.jpg" class="card-img-top" alt="Woofr">
         <div class="card-body">
@@ -110,8 +119,8 @@ function alertRegistrationSuccess() {
     </div>
     `;
 
-    pill_container.innerHTML = "";
-    pill_container.append(alertMessage);
+  pill_container.innerHTML = "";
+  pill_container.append(alertMessage);
 }
 
 //__________________________________________________________________________________
@@ -119,80 +128,87 @@ function alertRegistrationSuccess() {
 //__________________________________________________________________________________
 
 function handleAccountLogin(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // get data from user
-    const inputUsername = getInputValue("login_username");
-    const inputPassword = getInputValue("login-password");
+  // get data from user
+  const inputUsername = getInputValue("login_username");
+  const inputPassword = getInputValue("login-password");
 
-    // retrieve user database
-    const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-    
-        let requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
+  // retrieve user database
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    fetch(pantryAPIBasketUserUrl, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            if (inputUsername in data) {
-                if (data[inputUsername].password === inputPassword) {
-                    const userInfo = {"name": data[inputUsername].name, "username": inputUsername, "email": data[inputUsername].email};
-        
-                    localStorage.setItem("userInfo", JSON.stringify(userInfo));
-        
-                    alertRegistrationSuccess();
-                }else {
-                    alert("Invalid password!");
-                }
-            }else {
-                alert("Invalid username!");
-            }
-        })
-        .catch(error => console.log('error', error));
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(pantryAPIBasketUserUrl, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      if (inputUsername in data) {
+        if (data[inputUsername].password === inputPassword) {
+          const userInfo = {
+            name: data[inputUsername].name,
+            username: inputUsername,
+            email: data[inputUsername].email,
+          };
+
+          localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+          alertRegistrationSuccess();
+        } else {
+          alert("Invalid password!");
+        }
+      } else {
+        alert("Invalid username!");
+      }
+    })
+    .catch((error) => console.log("error", error));
 }
 
 //__________________________________Database Process________________________________
 
 function updateUserInfoToDatabase(userInfo) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    const object = {};
-    object[userInfo.username] = userInfo;
-    const raw = JSON.stringify(object);
+  const object = {};
+  object[userInfo.username] = userInfo;
+  const raw = JSON.stringify(object);
 
-    let requestOptions = {
-        method: 'PUT',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+  let requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    fetch(pantryAPIBasketUserUrl, requestOptions)
-    .catch(error => console.log('error', error));
+  fetch(pantryAPIBasketUserUrl, requestOptions).catch((error) =>
+    console.log("error", error)
+  );
 }
 
 function getUserInfoFromDatabase(result) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    let requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    fetch(pantryAPIBasketUserUrl, requestOptions)
-    .then(response => response.json())
-    .then(data => {result = data;})
-    .catch(error => console.log('error', error));
+  fetch(pantryAPIBasketUserUrl, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      result = data;
+    })
+    .catch((error) => console.log("error", error));
 }
 
 // helper function to get user input
 function getInputValue(id) {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
 }
--- js/my-account.js
+++ js/my-account.js
@@ -1,87 +1,112 @@
-const pantryAPIBasketPetsUrl = "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/PET_DATABASE";
+const pantryAPIBasketPetsUrl =
+  "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/PET_DATABASE";
 
 const username = JSON.parse(localStorage.getItem("userInfo")).username;
 
 document.getElementById("add_pet_form").addEventListener("submit", (e) => {
-    loadUserPetsData();
-    handleAddPetForm(e);
-})
+  e.preventDefault();
+  loadUserPetsData();
+  handleAddPetForm(e);
+});
 
-document.getElementById("add_pet_btn").addEventListener("click", (e => {
-    loadUserPetsData();
-}))
+// document.getElementById("add_pet_btn").addEventListener("click", (e) => {
+//   loadUserPetsData();
+// });
 
 let userPets = {};
 
 function getUserAndPetData() {
+  document.getElementById("user_account_info").innerHTML = `
 
 
 
-
-
-
-    document.getElementById("user_account_info").innerHTML = `
-    
-   
-   
-    `
-
+    `;
 }
 
 function loadUserPetsData() {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    let requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    fetch(pantryAPIBasketPetsUrl, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        userPets = data[username];
+  fetch(pantryAPIBasketPetsUrl, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      userPets = data[username];
     })
-    .catch(error => console.log('error', error));
+    .catch((error) => console.log("error", error));
 }
 
 function handleAddPetForm(e) {
-    e.preventDefault();
+  // get data from user
+  const addPetFormName = getInputValue("add_pet_form_name");
+  const addPetFormBreed = getInputValue("add_pet_form_breed");
+  const addPetFormAge = getInputValue("add_pet_form_age");
+  const addPetIsIntact = document.querySelector(
+    'input[name="add_pet_form_is_intact"]:checked'
+  ).value;
+  const addPetShotsUpToDate = document.querySelector(
+    'input[name="add_pet_form_shots_up_to_date"]:checked'
+  ).value;
 
-    // get data from user
-    const addPetFormName = getInputValue("add_pet_form_name");
-    const addPetFormBreed = getInputValue("add_pet_form_breed");
-    const addPetFormAge = getInputValue("add_pet_form_age");
-    const addPetIsIntact = document.querySelector('input[name="add_pet_form_is_intact"]:checked').value;
-    const addPetShotsUpToDate = document.querySelector('input[name="add_pet_form_shots_up_to_date"]:checked').value;
+  const petId = username + "-" + addPetFormName;
 
-    const petId = username + "-" + addPetFormName;
+  const userPetInfo = {
+    "pet-name": addPetFormName,
+    "pet-breed": addPetFormBreed,
+    "pet-age": addPetFormAge,
+    "pet-is-intact": addPetIsIntact,
+    "pet-shots-up-to-date": addPetShotsUpToDate,
+  };
 
-    const userPetInfo = {"pet-name": addPetFormName, "pet-breed": addPetFormBreed, "pet-age": addPetFormAge, "pet-is-intact": addPetIsIntact, "pet-shots-up-to-date": addPetShotsUpToDate};
+  userPets[petId] = userPetInfo;
+  const object = {};
+  object[username] = userPets;
+  const raw = JSON.stringify(object);
 
-    userPets[petId] = userPetInfo;
-    const object = {};
-    object[username] = userPets;
-    const raw = JSON.stringify(object);
+  // Initial staging for pushing data to pantry
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    // Initial staging for pushing data to pantry
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    let requestOptions = {
-        method: 'PUT',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+  let requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    fetch(pantryAPIBasketPetsUrl, requestOptions)
-    .catch(error => console.log('error', error));
+  fetch(pantryAPIBasketPetsUrl, requestOptions).catch((error) =>
+    console.log("error", error)
+  );
 }
-
 
 // helper function to get user input
 function getInputValue(id) {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
+}
+
+document.getElementById("see_pets").addEventListener("click", (e) => {
+  populateUserAndPetData(username);
+});
+
+function populateUserAndPetData(username) {
+  document.getElementById("user_account_info").innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <h5> ${username} </h5>
+    <ul id= "updated_pet_list" class="list-group list-group-flush">
+    </ul>
+  </div>
+  `;
+
+  for (petId in userPets) {
+    const petName = userPets[petId]["pet-name"];
+    const petlist = document.createElement("li");
+    petlist.classList.add("list-group-item");
+    petlist.textContent = petName;
+    document.getElementById("updated_pet_list").appendChild(petlist);
+  }
 }
