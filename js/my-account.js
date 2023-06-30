d85593979fd34bd8842bce9ff50c53c47c5fbd57
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-06-29 18:26:12 -0600

    my-account-cleaned

--- js/my-account.js
+++ js/my-account.js
@@ -1,7 +1,8 @@
 const pantryAPIBasketPetsUrl =
   "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/PET_DATABASE";
 
-const pantryAPIBasketPetServiceUrl = "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/PET_SERVICE_DATABASE"
+const pantryAPIBasketPetServiceUrl =
+  "https://getpantry.cloud/apiv1/pantry/0306b1cf-df37-49c7-bdbe-eb369a019f17/basket/PET_SERVICE_DATABASE";
 
 const username = JSON.parse(localStorage.getItem("userInfo")).username;
 let userPets = {};
@@ -17,14 +18,16 @@ document.addEventListener("DOMContentLoaded", () => {
         .addEventListener("click", (e) => populatePetServiceData(e));
     }
   });
-  
+
   loadPetServiceHistoryInAccount();
 });
 
-document.getElementById("add_pet_form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  handleAddPetForm(e);
-});
+addPetForm = document
+  .getElementById("add_pet_form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleAddPetForm(e);
+  });
 
 function loadUserPetsData() {
   const myHeaders = new Headers();
@@ -93,10 +96,10 @@ function handleAddPetForm(e) {
 function populatePetServiceData(e) {
   petId = e.target.getAttribute("id");
 
-  document.getElementById("booked_services").innerHTML = `
+  document.getElementById("booked_services").innerHTML += `
   <div class="card" style="width: 18rem;">
   <h3>${petId}</h3>
-  <ul id="booked_service_list" class="list-group list-group-flush"> 
+  <ul id="booked_service_list" class="list-group list-group-flush">
   </ul>
 </div>
   `;
@@ -110,27 +113,60 @@ function populatePetServiceData(e) {
 
 function populateUserAndPetData(username) {
   document.getElementById("user_account_info").innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <h5> ${username} </h5>
-    <ul id="updated_pet_list" class="list-group list-group-flush">
-    </ul>
-  </div>
+
+  <button id="userNameButton" class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  ${username}
+</button>
+<ul id="updated_pet_list" class="dropdown-menu dropdown-menu-dark"">
+</ul>
   `;
 
   for (petId in userPets) {
     const petName = userPets[petId]["pet-name"];
     const petlist = document.createElement("li");
-    petlist.classList.add("list-group-item");
-    petlist.classList.add("btn", "btn-primary");
-    petlist.setAttribute("id", petId);
-    petlist.textContent = petName;
+    petlist.classList.add("pet-li");
+
+    const actionLink = document.createElement("a");
+    actionLink.setAttribute("id", petId);
+    actionLink.classList.add("dropdown-item");
+    actionLink.href = "#";
+
+    actionLink.textContent = petName;
+
+    petlist.appendChild(actionLink);
     document.getElementById("updated_pet_list").appendChild(petlist);
   }
 }
 
-function getUserAndPetData() {
-  document.getElementById("user_account_info").innerHTML = ` 
-  `;
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+
+  setTimeout(() => {
+    wrapper.remove();
+  }, 2000);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    const formData = new FormData(document.getElementById("add_pet_form"));
+    const petName = document.getElementById("add_pet_form_name").value;
+    const petType = document.getElementById("add_pet_form_breed").value;
+    if (petName === "" || petType === "") {
+      appendAlert("Please fill in all required fields.", "danger");
+    } else {
+      appendAlert("Nice, you successfully added a pet!", "success");
+    }
+  });
 }
 
 // helper function to get user input
@@ -153,7 +189,7 @@ function loadPetServiceHistoryInAccount() {
   fetch(pantryAPIBasketPetServiceUrl, requestOptions)
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("serviceData", JSON.stringify(data))
+      localStorage.setItem("serviceData", JSON.stringify(data));
     })
     .catch((error) => console.log("error", error));
 }
